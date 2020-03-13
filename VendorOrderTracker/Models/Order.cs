2124b566e74f4f788652e14b3d05189ecdbcbd13
Drake Wilcox <drakewilcox@gmail.com>
2020-03-13 09:31:26 -0700

    create constructor and parameters to correspond with fields.

--- VendorOrderTracker/Models/Order.cs
+++ VendorOrderTracker/Models/Order.cs
@@ -11,7 +11,17 @@ namespace VendorOrderTracker.Models
     public string Date { get; set; }
     public int Id { get; }
     private static List<Order> _instances = new List<Order> { };
-    private static int _count = 0;
+    private static int _count = 1;
 
+    public Order(string title, string description, decimal price, string date)
+    {
+      Title = title;
+      Description = description;
+      Price = price;
+      Date = date;
+      _instances.Add(this);
+      Id = _count++;
+
+    }
   }
 }
