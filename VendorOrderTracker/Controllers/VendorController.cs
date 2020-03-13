d0bf0b402009e91f944a684b19a72bafb5bd7278
Drake Wilcox <drakewilcox@gmail.com>
2020-03-13 10:11:58 -0700

    add a SHOW route to controller to GET orders from a selected vendor.

--- VendorOrderTracker/Controllers/VendorController.cs
+++ VendorOrderTracker/Controllers/VendorController.cs
@@ -18,11 +18,23 @@ namespace VendorOrderTracker.Controllers
     {
       return View();
     }
+
     [HttpPost("/vendors")]
     public ActionResult Create(string name, string description, string location)
     {
       Vendor newVendor = new Vendor(name, description, location);
       return RedirectToAction("Index");
+    }
+
+    [HttpGet("/vendors/{id}")]
+    public ActionResult Show(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor selectedVendor = Vendor.Find(id);
+      List<Order> vendorOrders = selectedVendor.Orders;
+      model.Add("vendor", selectedVendor);
+      model.Add("orders", vendorOrders);
+      return View(model);
     }
 
 
