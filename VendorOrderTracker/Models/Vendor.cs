50fd8c8dc7ed6c96f400632246cf9d21ccac127e
Drake Wilcox <drakewilcox@gmail.com>
2020-03-13 09:09:32 -0700

    add initial fields to Vendor Model.

--- VendorOrderTracker/Models/Vendor.cs
+++ VendorOrderTracker/Models/Vendor.cs
@@ -5,6 +5,11 @@ namespace VendorOrderTracker.Models
 {
   public class Vendor
   {
-
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Location { get; set; }
+    public int Id { get; }
+    private static List<Vendor> _instances = new List<Vendor> { };
+    private static int _count = 0;
   }
 }
