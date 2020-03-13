ae14bdf0d5e4c924b82a1affb6ba38196ca42a45
Drake Wilcox <drakewilcox@gmail.com>
2020-03-13 09:28:15 -0700

    add necessary fields to Order Model.

--- VendorOrderTracker/Models/Order.cs
+++ VendorOrderTracker/Models/Order.cs
@@ -1,9 +1,17 @@
 using System.Collections.Generic;
+using System;
 
 namespace VendorOrderTracker.Models
 {
   public class Order
   {
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+    public string Date { get; set; }
+    public int Id { get; }
+    private static List<Order> _instances = new List<Order> { };
+    private static int _count = 0;
 
   }
 }
