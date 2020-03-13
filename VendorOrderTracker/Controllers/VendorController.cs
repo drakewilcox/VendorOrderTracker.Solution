acddba71ed381a4ac3250dde3c5dc23f2b173ac2
Drake Wilcox <drakewilcox@gmail.com>
2020-03-13 13:27:15 -0700

    add CREATE route to create new orders within vendor class.

--- VendorOrderTracker/Controllers/VendorController.cs
+++ VendorOrderTracker/Controllers/VendorController.cs
@@ -37,6 +37,19 @@ namespace VendorOrderTracker.Controllers
       return View(model);
     }
 
+    [HttpPost("/vendors/{vendorId}/orders")]
+    public ActionResult Create(int vendorId, string orderTitle, string orderDescription, decimal price, string date)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor foundVendor = Vendor.Find(vendorId);
+      Order newOrder = new Order(orderTitle, orderDescription, price, date);
+      foundVendor.AddOrder(newOrder);
+      List<Order> vendorOrders = foundVendor.Orders;
+      model.Add("orders", vendorOrders);
+      model.Add("vendor", foundVendor);
+      return View("Show", model);
+    }
+
 
   }
 }
