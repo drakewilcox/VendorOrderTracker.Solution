5188156bcb1267fd720a700723a909aa8974cc1a
Drake Wilcox <drakewilcox@gmail.com>
2020-03-13 13:44:53 -0700

    update paramater variable names to be specific to the Order class.

--- VendorOrderTracker/Controllers/VendorController.cs
+++ VendorOrderTracker/Controllers/VendorController.cs
@@ -38,11 +38,11 @@ namespace VendorOrderTracker.Controllers
     }
 
     [HttpPost("/vendors/{vendorId}/orders")]
-    public ActionResult Create(int vendorId, string orderTitle, string orderDescription, decimal price, string date)
+    public ActionResult Create(int vendorId, string orderTitle, string orderDescription, decimal orderPrice, string orderDate)
     {
       Dictionary<string, object> model = new Dictionary<string, object>();
       Vendor foundVendor = Vendor.Find(vendorId);
-      Order newOrder = new Order(orderTitle, orderDescription, price, date);
+      Order newOrder = new Order(orderTitle, orderDescription, orderPrice, orderDate);
       foundVendor.AddOrder(newOrder);
       List<Order> vendorOrders = foundVendor.Orders;
       model.Add("orders", vendorOrders);
